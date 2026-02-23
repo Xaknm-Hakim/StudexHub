0f58efc3c421a3cdede4e24dd00d24a95ed5a939
aizuddin <aizuddin1024@gmail.com>
2026-02-23 11:36:26 +0800

    X

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,1 +1,19 @@
+function readAndRedirect() {
+    // 1. Get the select element by its ID
+    var selectElement = document.getElementById("mySection");
+    
+    // 2. Read the value (the URL)
+    var selectedUrl = selectElement.value;
+    
+    // 3. Read the visible text (the Name)
+    var selectedText = selectElement.options[selectElement.selectedIndex].text;
+
+    // 4. Perform an action before redirecting
+    if (selectedUrl) {
+
+        window.location.href = selectedUrl; // Perform the redirect
+    } else {
+        alert("Please select a valid destination first!");
+    }
+}
 
