dce1ce3904d485364346ce1d4c71474bfa98b943
aizuddin <aizuddin1024@gmail.com>
2026-02-28 00:42:00 +0800

    finished login and signin

--- app/app/login/page.tsx
+++ app/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
   const [pass, setPass] = useState<string>("");
 
 async function handleLogin() {
-  const res = await fetch("/api/login", {
+  const res = await fetch("/api/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- app/app/signIn/page.tsx
+++ app/app/signIn/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
 
   
  async function handleSignup() {
-  const res = await fetch("/api/signup", {
+  const res = await fetch("api/auth/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
