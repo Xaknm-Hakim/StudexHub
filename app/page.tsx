d699809189b8b1d7b15613d4c35bb099acb7d158
aizuddin <aizuddin1024@gmail.com>
2026-02-27 17:17:50 +0800

    Restore deleted API auth routes from dev

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function MainPage() {
+
+    return(
+
+        <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-zinc-900 to-black text-white">
+            <div className="w-full max-w-md bg-zinc-900 p-8 rounded-xl space-y-4">        
+
+                    <h1 className="text-2xl text-center font-bold">Yo, welcome to StudexCenter</h1>
+                    <p className="text-sm text-center text-grey-400"> formerly known as BaruasHub (well it's still is lmao)</p>
+                    
+                    <div className="text-center text-sm text-gray-400">
+                      what you wanna do twin?{" "}
+                      <Link href="/signIn" className="text-white font-medium hover:underline">
+                        Sign in 
+                      </Link>
+                       -or- 
+                      <Link href="/login" className="text-white font-medium hover:underline">
+                        Log in
+                      </Link>
+                    </div>
+            </div>
+        </main>
+    );
+}
