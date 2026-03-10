bb9e14d9d0adc8803187ee038ce67c863ad524d0
aizuddin <aizuddin1024@gmail.com>
2026-03-11 01:25:50 +0800

    FINALLY FINSIHED/ noti not finished yet tho

--- app/app/schedules/page.tsx
+++ app/app/schedules/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import ScheduleForm from "@/components/ScheduleForm";
 import ScheduleTable from "@/components/ScheduleTable";
+import { useRouter } from "next/navigation";
 
 type ClassSchedule = {
   id: string;
@@ -16,7 +17,9 @@ type ClassSchedule = {
 
 export default function SchedulePage() {
 
+  const router = useRouter();
   const [schedules, setSchedules] = useState<ClassSchedule[]>([]);
+  const [editingSchedule, setEditingSchedule] = useState<ClassSchedule | null>(null);
 
   async function fetchSchedules() {
     const res = await fetch("/api/class-schedules");
@@ -37,11 +40,23 @@ export default function SchedulePage() {
 
   return (
     <main className="p-6 text-white">
+        <button
+          onClick={() => router.push("/dashboard")}
+          className="px-4 py-1 bg-black font-monospace text-white border-1 border-white rounded-lg cursor-pointer hover:bg-white hover:text-black transition duration-300 ease-in-out ">
+          Main Page
+        </button>
       <h1 className="text-2xl font-bold mb-6">Class Schedule</h1>
-      <ScheduleForm onSuccess={fetchSchedules} />
+
+      <ScheduleForm 
+      onSuccess={fetchSchedules}
+      editingSchedule={editingSchedule}
+      clearEdit={() => setEditingSchedule(null)}
+       />
+
       <ScheduleTable 
       schedules={schedules}
       onDelete={handleDelete}
+      onEdit={setEditingSchedule}
       />
     </main>
   );
