abd6ebe1c2ac281bb3b1923f17c38989de00a343
Zerpo-Zero <aizuddin1024@gmail.com>
2026-04-18 14:36:20 +0800

    Delete app/src/lib/types.ts
    Deleted types.ts that was conflicting with /app/src/lib/types

--- app/src/lib/types.ts
+++
@@ -1,8 +0,0 @@
-export type Priority = 'LOW' | "MEDIUM" | "HIGH";
-
-export type AssignmentFormData  = {
-    title: string;
-    dueDate: string;
-    priority?: Priority;
-    notes?: string | null;
-};
